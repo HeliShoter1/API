20ea850590a86c99df8988aebcb40027f5c32de2
HeliShoter1 <PhucCT.B21DVCN124@stu.ptit.edu.vn>
2023-10-18 22:22:03 +0700

    API

--- app/config.py
+++ app/config.py
@@ -1,2 +1,11 @@
 from pydantic import BaseSettings
 
+class Setting(BaseSettings):
+    dataBase_Hostname: str
+    database_port: str
+    database_password: str
+    database_name: str
+    database_username: str
+    secret_key: str
+    algorimthm: str
+    access_token_expire_minutes: int
