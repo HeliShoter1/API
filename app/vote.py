db2a118576688d9931b0b0d4cc0384a8a727ff86
HeliShoter1 <PhucCT.B21DVCN124@stu.ptit.edu.vn>
2023-10-22 14:53:21 +0700

    API

--- app/vote.py
+++ app/vote.py
@@ -11,6 +11,9 @@ router = APIRouter(
 
 @router.post('/',status_code= status.HTTP_201_CREATED)
 def create_vote(vote: Schemas.Vote,db: Session = Depends(get_db), cr_user : int = Depends(oauth2.get_current_user)):
+    post = db.query(models.Post).filter(models.Post.id == vote.post_id).first()
+    if post is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id: {vote.post_id} does not exist")
 
     vote_query = db.query(models.Vote).filter(models.Vote.post_id == vote.post_id,models.Vote.user_id == cr_user.id)
     fisrt_vote = vote_query.first()
