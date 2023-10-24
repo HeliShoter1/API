9543e227801e4e59d611792e48b209398069a877
HeliShoter1 <PhucCT.B21DVCN124@stu.ptit.edu.vn>
2023-10-24 19:22:30 +0700

    API

--- app/main.py
+++ app/main.py
@@ -2,10 +2,21 @@ from fastapi import *
 from typing import  List
 from . import  models, user, post, auth, vote
 from .database import *
+from fastapi.middleware.cors import CORSMiddleware
 
 # models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(user.router)
 app.include_router(post.router)
